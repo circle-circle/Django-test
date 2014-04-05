1a4d16528a06881b0bd5f81e6272d92b360cb04a
SeeYou <xuyuan10002720@163.com>
2014-04-05 13:48:55 +0800

    del hello

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, include, url
-from views import hello
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,6 +6,5 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^hello/$',include('hello')),
     url(r'^admin/', include(admin.site.urls)),
 )
